8a83956a04689b3c7993b4efe06d5e49c2a7bed2
Carlos Marin <carlosmarin151288@gmail.com>
2023-12-05 15:17:46 -0300

    Agregar las primeras rutas

---
+++ src/journal/routes/JournalRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { JorunalPage } from '../pages/JorunalPage'
+
+export const JournalRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<JorunalPage />} />
+            <Route path='/*' element={<Navigate to='/' />} />
+        </Routes>
+    )
+}
