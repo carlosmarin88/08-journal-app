9d77eba857bf88d5fb3576cf1680336d598fa01c
Carlos Marin <carlosmarin151288@gmail.com>
2023-12-07 10:12:28 -0300

    Agregar journal layout

--- src/journal/pages/JorunalPage.jsx
+++ src/journal/pages/JorunalPage.jsx
@@ -1,12 +1,13 @@
-import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JorunalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JorunalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>In sit ad esse velit Lorem sit consectetur cupidatat nisi in in. Sit aliquip cupidatat adipisicing dolor officia elit ullamco nulla anim ullamco ad proident officia. Tempor aliqua ullamco sunt commodo non laboris cillum ea incididunt pariatur ex anim sint proident.</Typography>
+      {/* NothinSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   )
 }
