12200542a768cccdd4d3cfe10968ac773e7fd325
Carlos Marin <carlosmarin151288@gmail.com>
2023-12-07 10:01:42 -0300

    Agregar contenido a la pagina RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,52 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const RegisterPage = () => {
     return (
-        <>
-            <h1> RegisterPage</h1>
-        </>
+
+        <AuthLayout title='Login'>
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label='Nombre completo'
+                            type='text'
+                            placeholder='Nombre completo'
+                            fullWidth /> {/** ocupar todo el ancho posible */}
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label='Correo'
+                            type='email'
+                            placeholder='correo@google.com'
+                            fullWidth /> {/** ocupar todo el ancho posible */}
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label='Contraseña'
+                            type='password'
+                            placeholder='Contraseña'
+                            fullWidth /> {/** ocupar todo el ancho posible */}
+                    </Grid>
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={12}>
+                            <Button variant='contained' fullWidth>
+                                CREAR CUENTA
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid container direction='row' justifyContent='end'>
+                        <Typography sx={{mr: 1}}>¿Ya tienes cuentas? </Typography>
+                        <Link
+                            component={RouterLink}
+                            color='inherit' to='/auth/login'>
+                            Ingresar
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
     )
 }
