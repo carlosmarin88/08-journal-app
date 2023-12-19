b20af12e6659b099a4fa00ecc2df0893180e8030
Carlos Marin <carlosmarin151288@gmail.com>
2023-12-19 10:15:29 -0300

    Agregar manejo de formulario de login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,18 +3,48 @@ import { Google } from '@mui/icons-material'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
+import { useDispatch } from 'react-redux'
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth'
 
 export const LoginPage = () => {
+
+
+    const dispatch = useDispatch()
+
+    const {email, password, onInputChange} = useForm({
+        email: 'charly@google.com',
+        password: '123456'
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        console.log({
+            email,
+            password
+        })
+        dispatch(checkingAuthentication(email, password))
+    }
+
+    const onGoogleSignIn = () => {
+        console.log('onGoogleSignIn')
+        dispatch(startGoogleSignIn())
+    }
+
     return (
 
+
         <AuthLayout title='Login'>
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField
                             label='Correo'
                             type='email'
                             placeholder='correo@google.com'
+                            name='email' // lo usamos para el hook useForm
+                            value={email}
+                            onChange={onInputChange}
                             fullWidth /> {/** ocupar todo el ancho posible */}
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,16 +52,22 @@ export const LoginPage = () => {
                             label='Contraseña'
                             type='password'
                             placeholder='Contraseña'
+                            name='password'
+                            value={password}
+                            onChange={onInputChange}
                             fullWidth /> {/** ocupar todo el ancho posible */}
                     </Grid>
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
                         <Grid item xs={12} sm={6}>
-                            <Button variant='contained' fullWidth>
+                            <Button type='submit' variant='contained' fullWidth>
                                 Login
                             </Button>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Button variant='contained' fullWidth>
+                            <Button 
+                            variant='contained' 
+                            onClick={onGoogleSignIn}
+                            fullWidth>
                                 <Google />
                                 <Typography sx={{ ml: 1 }}>Google</Typography>
                             </Button>
---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMC_A19jpFuqZS8aMaPCK5Sz1fx1uqCho",
+  authDomain: "react-cursos-fa14c.firebaseapp.com",
+  projectId: "react-cursos-fa14c",
+  storageBucket: "react-cursos-fa14c.appspot.com",
+  messagingSenderId: "1084853504802",
+  appId: "1:1084853504802:web:e15baae832dc8e27adf37b",
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig)
+
+export const FirebaseAuth = getAuth(FirebaseApp)
+
+export const FirebaseDB = getFirestore(FirebaseApp)
