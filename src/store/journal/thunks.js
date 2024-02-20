48a3fa281a491394b732fedcdebb55c827ee067b
Carlos Marin <carlosmarin151288@gmail.com>
2024-02-20 11:40:41 -0300

    Traer las notas y guardarlas en el store

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite"
 import { FirebaseDB } from "../../firebase/config"
-import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice"
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./journalSlice"
+import { loadNotes } from "../../helpers"
 
 export const startNewNote = () => {
     return async(dispatch, getState) => {
@@ -27,8 +28,16 @@ export const startNewNote = () => {
         newNote.id = newDoc.id
         dispatch(addNewEmptyNote(newNote));
         dispatch(setActiveNote(newNote));
-        //! dispatch
-        // dispatch( newNote )
-        // dispatch( activeNote )
+        
+    }
+}
+
+export const startLoadingNotes = () => {
+    return async(dispatch, getState) => {
+        const { uid } = getState().auth
+        if(!uid) throw new Error('El UID del usuario no existe')
+        //console.log({uid})
+        const notes = await loadNotes(uid)
+        dispatch(setNotes(notes))
     }
 }
