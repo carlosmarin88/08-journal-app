d76ec8ac2e3ffa59a8547fe36abd664dd6306061
Carlos Marin <33185069+carlosmarin88@users.noreply.github.com>
2024-03-14 17:13:35 -0300

    agregar configuracion de firebase en las enviroment

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,19 +17,39 @@ import { getEnviroments } from "../helpers";
 //   appId: "1:1084853504802:web:e15baae832dc8e27adf37b",
 // };
 
-const env = getEnviroments();
-console.log(env);
+const {
+VITE_APIKEY,
+VITE_AUTH_DOMIAN,
+VITE_PROJECT_ID,
+VITE_STORAGE_BUCKET,
+VITE_MESSAGING_SENDER_ID,
+VITE_APP_ID,
+VITE_MEASUREMENT_ID
+} = getEnviroments();
 
 // Testing
-const firebaseConfig = {
-  apiKey: "AIzaSyDLnd7Wa2OwY1DTejoY0Xv4CoWkq551KXI",
-  authDomain: "base-test-45baf.firebaseapp.com",
-  projectId: "base-test-45baf",
-  storageBucket: "base-test-45baf.appspot.com",
-  messagingSenderId: "520182608458",
-  appId: "1:520182608458:web:b7bedb4371e5b58ba06cb3",
-  measurementId: "G-K273PVWET5"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDLnd7Wa2OwY1DTejoY0Xv4CoWkq551KXI",
+//   authDomain: "base-test-45baf.firebaseapp.com",
+//   projectId: "base-test-45baf",
+//   storageBucket: "base-test-45baf.appspot.com",
+//   messagingSenderId: "520182608458",
+//   appId: "1:520182608458:web:b7bedb4371e5b58ba06cb3",
+//   measurementId: "G-K273PVWET5"
+// };
+
+ const firebaseConfig = {
+   apiKey: VITE_APIKEY,
+   authDomain: VITE_AUTH_DOMIAN,
+   projectId: VITE_PROJECT_ID,
+   storageBucket: VITE_STORAGE_BUCKET,
+   messagingSenderId: VITE_MESSAGING_SENDER_ID,
+   appId: VITE_APP_ID, 
+  };
+
+  if(typeof VITE_MEASUREMENT_ID !== undefined) firebaseConfig.measurementId = VITE_MEASUREMENT_ID;
+
+  console.log('firebaseConfig', firebaseConfig);
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig)
